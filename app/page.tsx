501d875fbf67d580068aed2bbf958418e8427f8c
Aaroh Sharma <aaroh.sh@gmail.com>
2025-08-11 20:33:32 -0500

    Add vercel analytics

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import HeroSection from './components/HeroSection';
 import SystemArchitecture from './components/SystemArchitecture';
 import TechnicalMetrics from './components/TechnicalMetrics';
 import ContactSection from './components/ContactSection';
+import { Analytics } from "@vercel/analytics/next"
 
 export default function BackendPortfolio() {
 	return (
 		<main className="min-h-screen bg-[#1A1A1A] text-white overflow-x-hidden">
+            <Analytics/>
 			<HeroSection />
 			<SystemArchitecture />
 			<TechnicalMetrics />
